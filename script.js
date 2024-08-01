43771888c726a5a18f00d656968349025d6b1876
teleniyzer <teleniyzer2007@gmail.com>
2024-08-01 12:36:22 +0200

    add something in script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+//we are make a cats (=^.^=)
 var drawCats = function (howManyTimes) {
   for (var i = 0; i < howManyTimes; i++) {
-      console.log(i + " =^.^=");
+      console.log(i + "(=^.^=)");
   }
 };
-drawCats(3);
+drawCats(3); //misto 3 tady muze byt kolik chces
 
 //sdsdsd
+
+/*frfrfrfurjfurfurfj
+frfr
+
+console.log('fggfgfgf')
+rf
+rf
+loreem
+f
+gfg
+rf
+r
+fr
+fr
+r
+*/
+console.log('fggfgfgf')
